c9d06ceb5689cbf3678381b7b873c1eabaf29b8f
Amit Patange <amitpatange88@gmail.com>
2018-09-24 23:22:48 +0530

    first commit

---
+++ WhatsappDemo/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WhatsAppApi;
+
+namespace WhatsappDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Message = "Hi there, Sample test message sent.";
+            SendMessage("917709981177", Message);
+            Console.ReadLine();
+
+        }
+
+        private static string SendMessage(String To, String Message)
+        {
+            string status = string.Empty;
+            WhatsApp wa = new WhatsApp("917709981177", "352038068257991", "Amit", false, false);
+            wa.OnConnectSuccess += () =>
+            {
+                wa.OnLoginSuccess += (phonenumber, data) =>
+                {
+                    status = "Connection Success";
+                    wa.SendMessage(To, Message);
+                    status = "Message sent Success";
+                };
+
+                wa.OnLoginFailed += (data) =>
+                {
+                    status = "Login Failed" + data;
+                };
+                wa.Login();
+            };
+            wa.OnConnectFailed += (ex) =>
+            {
+                status = "Connection Failed" + ex.StackTrace;
+            };
+            wa.Connect();
+
+            return status;
+        }
+    }
+}
